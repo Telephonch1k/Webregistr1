02ddd6c7438cd09807391ba447819710e6a0b7df
Екатерина Сергеевна <katedesu@mail.ru>
2021-10-26 16:20:09 +0500

    начало4 видео

---
+++ Webregistr1/Models/AppCtx.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webregistr1.Models
+{
+    public class AppCtx : IdentityDbContext<User>
+    {
+        public AppCtx(DbContextOptions<AppCtx> options)
+            : base(options)
+    {
+        Database.EnsureCreated(); 
+    }
+}
+}
---
+++ Webregistr1/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Webregistr1.Models
+{
+    public class User : IdentityUser
+    {
+        //Дополнительные поля для пользователей
+        //Для преподавателя погут понадобиться данные о ФИО
+
+        //Сообщение об ошибке при валидации на стороне клиента
+        [Required(ErrorMessage = "Введите фамилию")]
+
+        //Отображение фамилии вместо LastName
+        [Display(Name = "Фамилия")]
+
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Введите имя")]
+        [Display(Name = "Имя")]
+
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Введите отчество")]
+        [Display(Name = "Отчество")]
+
+        public string Patronymic { get; set; }
+
+       //Навигационные свойства
+    }
+}
